627e6622aa37ce13e56042ebf56ab1406b5a0550
Fabián Fernández <fabianfdez06@gmail.com>
2024-09-04 22:21:16 -0600

    feat: update project with new base structure

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,9 +1,12 @@
-﻿namespace MiniPython
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MiniPythonCompilerGUI;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App
-    {
-    }
 }
