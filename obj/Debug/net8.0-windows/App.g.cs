527df507f8b28e1591e3a22ff8ef0355e565f6cf
Kevin Jiménez <67296979+Khraben@users.noreply.github.com>
2024-09-04 22:41:54 -0600

    Add Antlr4

--- obj/Debug/net8.0-windows/App.g.cs
+++ obj/Debug/net8.0-windows/App.g.cs
@@ -44,7 +44,7 @@ namespace MiniPythonCompilerGUI {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.7.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -59,7 +59,7 @@ namespace MiniPythonCompilerGUI {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.7.0")]
         public static void Main() {
             MiniPythonCompilerGUI.App app = new MiniPythonCompilerGUI.App();
             app.InitializeComponent();
